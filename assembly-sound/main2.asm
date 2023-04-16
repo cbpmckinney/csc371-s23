d03be7b87dbcf0de024741425222140ce8fae96e
Colin McKinney <42683765+cbpmckinney@users.noreply.github.com>
2023-04-16 19:56:28 -0400

    Started irq based sound example

---
+++ assembly-sound/main2.asm
@@ -0,0 +1,37 @@
+* = $0801
+; SYS(8192) instead of SYS(4096)
+.byte $08, $0d, $0a, $00, $9e, $28, $38
+.byte $31, $39, $32, $29, $00, $00, $00
+
+* = $1000
+gameDataSID
+
+.binary "Calypso_Bar.sid", $7e
+
+.include "libSound2.asm"
+
+* = $2000
+
+; Call the Libsound initialization
+#LIBSOUND_INIT_A gameDataSID
+; Set up custom IRQ handler for timings
+initirq
+
+
+
+mainloop
+
+    #LIBSOUND_UPDATE_A gameDataSID
+    jsr delay
+
+jmp mainloop
+
+delay
+ldx #255
+delayloop
+cpx #0
+beq delayexit
+dex 
+jmp delayloop
+delayexit
+rts
